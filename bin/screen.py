7ed2cbe1ab80c2a051f731c4474a9141c4a9bfa0
trying git <pisaq11@gmail.com>
2026-08-16 14:30:03 +0300

    screen

--- bin/screen.py
+++ bin/screen.py
@@ -22,12 +22,32 @@ def bush():
     image = pygame.image.load('grass.png')
     for i in range(20):
         img_small = pygame.transform.scale(image, (40, 30))
-        x = random.randint(0,920)
-        y = random.randint(0,440)
+        x = random.randint(40,920)
+        y = random.randint(80,440)
         screen.blit(img_small, (x, y))
     #screen.blit(image, (100, 100))
     pygame.display.flip()
     return screen
+
+def screen_net():
+    screen = blank_screen()
+    color_line = consts.BACKGROUND_COLOR_GREEN
+    for i in range(25):
+        y = i*20
+        pygame.draw.line(screen, color_line, (0, y), (1000, y))
+        pygame.display.flip()
+    for i in range(1000):
+        x = i*20
+        pygame.draw.line(screen, color_line, (x, 0), (x, 500))
+        pygame.display.flip()
+    return screen
+
+def bomb():
+    screen = blank_screen()
+    image = pygame.image.load('mine.png')
+    for i in range(20):
+        img_small = pygame.transform.scale(image, (60, 20))
+
 
 
 
@@ -39,4 +59,5 @@ def display_screen(screen):
             if event.type == pygame.QUIT:
                 running = False
 
-#display_screen(bush())
+display_screen(bush())
+display_screen(screen_net())
