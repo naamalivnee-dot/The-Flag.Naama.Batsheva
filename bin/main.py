90b66ee2c8a07c38f5d872478cd19e904b36c36e
new game <naamalivnee@gmail.com>
2026-08-16 12:23:29 +0300

    new cons

--- bin/main.py
+++ bin/main.py
@@ -1,1 +1,20 @@
+import sys
+
 import pygame
+import consts
+import game_field
+import screen
+import soldier
+
+
+
+move_f
+
+
+def handle_user_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == pygame.KEYDOWN:
