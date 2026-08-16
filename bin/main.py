91625229f5832c1f0316847f142b07287a5d0b16
trying git <pisaq11@gmail.com>
2026-08-16 14:59:07 +0300

    screen

--- bin/main.py
+++ bin/main.py
@@ -5,6 +5,13 @@ import game_field
 import screen
 import soldier
 
+def location_x_on_screen(i):
+    x= 20*i
+
+
+def location_y_on_screen(j):
+    y = j * 20
+    return y
 
 
 def main():
--- bin/screen.py
+++ bin/screen.py
@@ -3,10 +3,7 @@ import consts
 import random
 
 def blank_screen():
-    #background_color = consts.BACKGROUND_COLOR_GREEN
     screen = pygame.display.set_mode((1000,500))
-    #pygame.display.set_caption(consts.GAME_OPENING_TEXT_FOR_SOLDIER)
-    #screen.fill(background_color)
     pygame.display.flip()
     return screen
 
@@ -22,34 +19,25 @@ def bush():
     image = pygame.image.load('grass.png')
     for i in range(20):
         img_small = pygame.transform.scale(image, (40, 30))
-        x = random.randint(40,920)
-        y = random.randint(80,440)
+        x = random.randint(0,920)
+        y = random.randint(0,440)
         screen.blit(img_small, (x, y))
     #screen.blit(image, (100, 100))
     pygame.display.flip()
     return screen
 
-def screen_net():
+def net_screen():
     screen = blank_screen()
     color_line = consts.BACKGROUND_COLOR_GREEN
-    for i in range(25):
-        y = i*20
-        pygame.draw.line(screen, color_line, (0, y), (1000, y))
+    for i in range(500):
+        x = i * 20
+        pygame.draw.line(screen, color_line, (x, 0), (x, 1000))
         pygame.display.flip()
     for i in range(1000):
-        x = i*20
-        pygame.draw.line(screen, color_line, (x, 0), (x, 500))
+        y = i * 20
+        pygame.draw.line(screen, color_line, (0, y), (1000,y))
         pygame.display.flip()
     return screen
-
-def bomb():
-    screen = blank_screen()
-    image = pygame.image.load('mine.png')
-    for i in range(20):
-        img_small = pygame.transform.scale(image, (60, 20))
-
-
-
 
 
 def display_screen(screen):
@@ -60,4 +48,4 @@ def display_screen(screen):
                 running = False
 
 display_screen(bush())
-display_screen(screen_net())
+display_screen(net_screen())
