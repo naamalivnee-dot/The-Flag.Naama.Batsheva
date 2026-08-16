7d0067ce2644adb212799915096f32312a05097a
trying git <pisaq11@gmail.com>
2026-08-16 16:28:39 +0300

    NEW CONSTS

--- bin/screen.py
+++ bin/screen.py
@@ -2,6 +2,9 @@ import pygame
 import consts
 import random
 
+from bin import game_field
+
+
 def blank_screen():
     screen = pygame.display.set_mode((consts.WINDOW_WIDTH,consts.WINDOW_HEIGHT))
     pygame.display.flip()
@@ -14,13 +17,15 @@ def green_screen():
     pygame.display.flip()
     return screen
 
-def bush():
+def grass():
     screen = green_screen()
     image = pygame.image.load('grass.png')
+    grass_height = consts.GRASS_HEIGHT * consts.CONVERT_TO_SCREEN
+    grass_width = consts.GRASS_WIDTH * consts.CONVERT_TO_SCREEN
     for i in range(consts.CONVERT_TO_SCREEN):
-        img_small = pygame.transform.scale(image, (consts.GRASS_HEIGHT, consts.GRASS_WIDTH))
-        x = random.randint(consts.MAN_WIDTH, consts.WINDOW_WIDTH - consts.GRASS_WIDTH)
-        y = random.randint(consts.MAN_HEIGHT, consts.WINDOW_HEIGHT - consts.GRASS_HEIGHT)
+        img_small = pygame.transform.scale(image, (grass_height, grass_width))
+        x = random.randint(0, consts.WINDOW_WIDTH - grass_width)
+        y = random.randint(0, consts.WINDOW_HEIGHT - grass_height)
         screen.blit(img_small, (x, y))
     #screen.blit(image, (100, 100))
     pygame.display.flip()
@@ -32,18 +37,45 @@ def net_screen():
     for i in range(consts.WINDOW_HEIGHT):
         x = i * consts.CONVERT_TO_SCREEN
         pygame.draw.line(screen, color_line, (x, 0), (x, consts.WINDOW_WIDTH))
-        pygame.display.flip()
+       # pygame.display.flip()
     for i in range(consts.WINDOW_WIDTH):
         y = i * consts.CONVERT_TO_SCREEN
         pygame.draw.line(screen, color_line, (0, y), (consts.WINDOW_WIDTH, y))
-        pygame.display.flip()
+    pygame.display.flip()
     return screen
 
 
-def mine_screen():
+def mine_screen(met):
     screen = net_screen()
     image = pygame.image.load('mine.png')
-    img_small = pygame.transform.scale(image, (consts.MINE_HEIGHT, consts.MINE_WIDTH))
+    mine_height = consts.MINE_HEIGHT * consts.CONVERT_TO_SCREEN
+    mine_width = consts.MINE_WIDTH * consts.CONVERT_TO_SCREEN
+    img_small = pygame.transform.scale(image, (mine_width, mine_height))
+    for i in range(len(met)):
+        for j in range(len(met[i])):
+            if met[i][j] == consts.MINE_SQUARE:
+                x = i * consts.CONVERT_TO_SCREEN
+                y = j * consts.CONVERT_TO_SCREEN
+                screen.blit(img_small, (x, y))
+    pygame.display.flip()
+    return screen
+
+
+'''def flag_on_screen(met):
+    screen = grass()
+    image = pygame.image.load('flag.png')
+    flag_height = consts.FLAG_HEIGHT * consts.CONVERT_TO_SCREEN
+    flag_width = consts.FLAG_WIDTH * consts.CONVERT_TO_SCREEN
+    img_small = pygame.transform.scale(image, (flag_width, flag_height))
+    for i in range(len(met)):
+        for j in range(len(met[i])):
+            if met[i][j] == consts.FLAG_SQUARE:
+                x = i * consts.CONVERT_TO_SCREEN
+                y = j * consts.CONVERT_TO_SCREEN
+                screen.blit(img_small, (x, y))
+    pygame.display.flip()
+    return screen'''
+
 
 
 
@@ -55,5 +87,20 @@ def display_screen(screen):
             if event.type == pygame.QUIT:
                 running = False
 
-display_screen(bush())
+display_screen(grass())
 display_screen(net_screen())
+
+'''def main_grass_screen():
+    game_board = game_field.create_board()
+    met = game_field.Flag_position(game_board)
+    display_screen(flag_on_screen(met))
+    pass
+main_grass_screen()'''
+
+
+def main_net_screen():
+    game_board = game_field.create_board()
+    met = game_field.Placing_mines_in_game_board(game_board)
+    display_screen(mine_screen(met))
+main_net_screen()
+
