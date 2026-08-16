809231e4d0450e71ea1508d4492f73774fd552be
new game <naamalivnee@gmail.com>
2026-08-16 11:48:45 +0300

    new

---
+++ bin/consts.py
@@ -0,0 +1,13 @@
+#Constant variables in our game
+ROWS_ON_THE_GAME_BOARD=25
+COLUMNS_ON_THE_GAME_BOARD=50
+
+GAME_OPENING_TEXT_FOR_SOLDIER="Welcome to the game \n have fun!"
+print(GAME_OPENING_TEXT_FOR_SOLDIER)
+#window_size
+#WINDOW_HEIGHT=
+#WINDOW_WIDTH=
+
+#BACKGROUND_COLOR_GREEN = (, ,)
+
+
