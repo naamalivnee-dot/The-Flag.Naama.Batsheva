5a97ce22c7befacddfd93a4511ca710c96bca045
new game <naamalivnee@gmail.com>
2026-08-16 14:07:51 +0300

    new main

--- bin/main.py
+++ bin/main.py
@@ -1,29 +1,32 @@
 import sys
-
 import pygame
 import consts
 import game_field
 import screen
 import soldier
 
-
-
-move_f
-
-
 def handle_user_events():
+    print("press")
+    pygame.key.get_pressed()
+    pygame.init()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        #elif state["state"] != consts.RUNNING_STATE:
+            #continue
+        if event.type == KEYDOWN:
+            if (pygame.K_LEFT == event.key):
+                print("left")
+            elif (pygame.K_RIGHT == event.key):
+                print("right")
+            elif (pygame.K_UP == event.key):
+                print("up")
+            elif (pygame.K_DOWN == event.key):
+                print("down")
 
-        if event.type == pygame.KEYDOWN:
-            if event.type == KEYDOWN:
-                if (event.key == pygame.K_LEFT):
-                    sprite = pygame.image.load('left.png')
-                elif (event.key == pygame.K_RIGHT):
-                    sprite = pygame.image.load('right.png')
-                elif (event.key == pygame.K_UP):
-                    sprite = pygame.image.load('up.png')
-                elif (event.key == pygame.K_DOWN):
-                    sprite = pygame.image.load('down.png')
+
+def main():
+    handle_user_events()
+main()
