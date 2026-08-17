01cd9b945ae6d8746bbcca1c8a7f12d91e0159f7
new game <naamalivnee@gmail.com>
2026-08-17 09:58:57 +0300

    new board

--- bin/soldier.py
+++ bin/soldier.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
-import consts
+import const
+
 from bin import game_field
 
 pygame.init()
@@ -35,12 +36,8 @@ def handle_user_events(row,col):
 
     return row,col
 
-def moving_soldier_piece():
-    for row in range(consts.ROWS_ON_THE_GAME_BOARD_ON_THE_GAME_BOARD):
-        for col in range(consts.COLUMNS_ON_THE_GAME_BOARD) :
-            if game_field.game_board[row][col] == consts.SOLDIER_WIDTH and game_field.game_board[row][col+1]==NO_MINE and game_field.game_board[row+1][col]==NO_MINE:
-                game_field.game_board[row][col]=consts.SOLDIER_MOVING_SQUARE
-                print(game_board)
+def soldier_touches_the_flag():
+    if game_field.game_board:
 
 
 
