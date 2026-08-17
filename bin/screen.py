25d829ecfc6b1b7106fac153fba609c7c048ad0f
trying git <pisaq11@gmail.com>
2026-08-17 11:36:38 +0300

    screen and consts

--- bin/screen.py
+++ bin/screen.py
@@ -27,7 +27,6 @@ def grass():
         x = random.randint(0, consts.WINDOW_WIDTH - grass_width)
         y = random.randint(0, consts.WINDOW_HEIGHT - grass_height)
         screen.blit(img_small, (x, y))
-    #screen.blit(image, (100, 100))
     pygame.display.flip()
     return screen
 
@@ -37,7 +36,6 @@ def net_screen():
     for i in range(consts.WINDOW_HEIGHT):
         x = i * consts.CONVERT_TO_SCREEN
         pygame.draw.line(screen, color_line, (x, 0), (x, consts.WINDOW_WIDTH))
-       # pygame.display.flip()
     for i in range(consts.WINDOW_WIDTH):
         y = i * consts.CONVERT_TO_SCREEN
         pygame.draw.line(screen, color_line, (0, y), (consts.WINDOW_WIDTH, y))
@@ -45,7 +43,7 @@ def net_screen():
     return screen
 
 
-def mine_screen(met):
+def mine(met):
     screen = net_screen()
     image = pygame.image.load('mine.png')
     mine_height = consts.MINE_HEIGHT * consts.CONVERT_TO_SCREEN
@@ -73,6 +71,42 @@ def flag():
     pygame.display.flip()
     return screen
 
+def draw_soldier(met):
+    screen = flag()
+    image = pygame.image.load('soldier.png')
+    soldier_height = consts.SOLDIER_HEIGHT * consts.CONVERT_TO_SCREEN
+    soldier_width = consts.SOLDIER_WIDTH * consts.CONVERT_TO_SCREEN
+    img_small = pygame.transform.scale(image, (soldier_width, soldier_height))
+    for i in range(len(met)):
+        for j in range(len(met[i])):
+            if met[i][j] == consts.SOLDIER_MOVING_SQUARE:
+                y = i
+                x = j
+                screen.blit(img_small, (x, y))
+    pygame.display.flip()
+    return screen
+
+
+
+
+def draw_night_soldier(met):
+    screen = mine_screen()
+    image = pygame.image.load('soldier_nigth.png')
+    soldier_height = consts.SOLDIER_HEIGHT * consts.CONVERT_TO_SCREEN
+    soldier_width = consts.SOLDIER_WIDTH * consts.CONVERT_TO_SCREEN
+    img_small = pygame.transform.scale(image, (soldier_width, soldier_height))
+    for i in range(len(met)):
+        for j in range(len(met[i])):
+            if met[i][j] == consts.SOLDIER_SQUARE:
+                y = i
+                x = j
+                screen.blit(img_small, (x, y))
+    pygame.display.flip()
+    return screen
+
+
+
+
 
 
 
@@ -84,13 +118,32 @@ def display_screen(screen):
             if event.type == pygame.QUIT:
                 running = False
 
-display_screen(flag())
-display_screen(net_screen())
+#display_screen(flag())
+display_screen(draw_soldier(game_field.Updated_board()))
 
 
 
-def main_net_screen():
+def mine_screen():
     game_board = game_field.create_board()
-    met = game_field.Placing_mines_in_game_board(game_board)
-    display_screen(mine_screen(met))
-main_net_screen()
+    met = game_field.Updated_board()
+    return mine(met)
+    #display_screen(mine_screen(met))
+#display_screen(mine_screen())
+#main_net_screen()
+
+display_screen(draw_night_soldier(game_field.Updated_board()))
+
+
+#def winning_screen():
+    #screen = draw_soldier(game_field.Updated_board())
+    #pygame.init()
+    #font = pygame.font.Font('freesansbold.ttf', 100)
+    #text = font.render('you win!', True, consts.WHITE)
+    #screen.blit(text, (consts.WINDOW_HEIGHT/2, consts.WINDOW_WIDTH/2))
+    #pygame.display.update()
+    #return screen
+
+
+#display_screen(winning_screen())
+
+
